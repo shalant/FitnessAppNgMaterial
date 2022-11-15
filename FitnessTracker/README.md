18c4814aa26a5332993049cc98767b457faff7ee
Douglas Rosenberg <doug.rosenberg@gmail.com>
2022-11-15 11:04:01 -0600

    working on basic material ui

--- FitnessTracker/README.md
+++ FitnessTracker/README.md
@@ -1,4 +1,4 @@
-# FitnessApp
+# FitnessTracker
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.2.8.
 
--- FitnessTracker/src/app/app.module.ts
+++ FitnessTracker/src/app/app.module.ts
@@ -1,26 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductComponent } from './product/product.component';
-import { ProductsComponent } from './products.component';
-import { ProductsService } from './products.service';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProductComponent,
-    ProductsComponent,
-
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
-  providers: [ProductsService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
