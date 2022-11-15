93490788039353bd3c19b911f1c46cd9df9dddd7
Douglas Rosenberg <doug.rosenberg@gmail.com>
2022-11-14 22:23:14 -0600

    initial

---
+++ FitnessApp/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { ProductsComponent } from './products.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductComponent,
+    ProductsComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
