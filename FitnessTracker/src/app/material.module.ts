71e43162e886bde8723ee59de5e1140f7f707f44
Douglas Rosenberg <doug.rosenberg@gmail.com>
2022-11-15 22:21:58 -0600

    working on navbar

--- FitnessTracker/src/app/material.module.ts
+++ FitnessTracker/src/app/material.module.ts
@@ -6,6 +6,8 @@ import {MatInputModule} from '@angular/material/input';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 // import {MatNativeDateModule} from '@angular/material/core;
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
     imports: [
@@ -14,7 +16,9 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
         MatFormFieldModule,
         MatInputModule,
         MatDatepickerModule,
-        MatCheckboxModule
+        MatCheckboxModule,
+        MatSidenavModule,
+        MatToolbarModule
     ],
     exports: [
         MatButtonModule,
@@ -22,7 +26,9 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
         MatFormFieldModule,
         MatInputModule,
         MatDatepickerModule,
-        MatCheckboxModule
+        MatCheckboxModule,
+        MatSidenavModule,
+        MatToolbarModule
     ]
 })
 export class MaterialModule {}
