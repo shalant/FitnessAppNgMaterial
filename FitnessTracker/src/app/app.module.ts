92ca477dbd62d7aa9c82a74047960d01257b8060
Douglas Rosenberg <doug.rosenberg@gmail.com>
2022-11-16 22:23:00 -0600

    working on completed exercise table

--- FitnessTracker/src/app/app.module.ts
+++ FitnessTracker/src/app/app.module.ts
@@ -19,6 +19,7 @@ import { HeaderComponent } from './navigation/header/header.component';
 import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
 import { StopTrainingComponent } from './training/current-training/stop-training.component';
 import { AuthService } from './auth/auth.service';
+import { TrainingService } from './training/training.service';
 
 
 @NgModule({
@@ -46,7 +47,7 @@ import { AuthService } from './auth/auth.service';
     MatNativeDateModule,
     ReactiveFormsModule,
   ],
-  providers: [AuthService],
+  providers: [AuthService, TrainingService],
   bootstrap: [AppComponent],
   entryComponents: [StopTrainingComponent]
 })
--- FitnessTracker/src/app/material.module.ts
+++ FitnessTracker/src/app/material.module.ts
@@ -14,7 +14,7 @@ import {MatCardModule} from '@angular/material/card';
 import {MatSelectModule} from '@angular/material/select';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatDialogModule} from '@angular/material/dialog';
-
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
     imports: [
@@ -31,7 +31,8 @@ import {MatDialogModule} from '@angular/material/dialog';
         MatCardModule,
         MatSelectModule,
         MatProgressSpinnerModule,
-        MatDialogModule
+        MatDialogModule,
+        MatTableModule
     ],
     exports: [
         MatButtonModule,
@@ -47,7 +48,8 @@ import {MatDialogModule} from '@angular/material/dialog';
         MatCardModule,
         MatSelectModule,
         MatProgressSpinnerModule,
-        MatDialogModule
+        MatDialogModule,
+        MatTableModule
     ]
 })
 export class MaterialModule {}
